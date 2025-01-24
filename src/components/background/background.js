1eb00ed02818e5c023885de934bfd94720b12ea5
Sumit <nupzmit@Nupzmits-MacBook-Pro.local>
2025-01-24 22:30:32 +0100

    Add background sound on click of background

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -1,10 +1,23 @@
+import backgroundAudio from './background-1.mp3';
+import { useRef } from "react";
 import "./background.css";
 
 function Background() {
+    const audioRef = useRef(null);
+
+    const playAudio = () => {
+        if (!audioRef.current.playing) {
+            audioRef.current.play();
+        }
+    };
+
     return (
-        <div className="game-background">
+        <div className="game-background" onClick={() => playAudio()}>
             <div className="background-layer"></div>
             <div className="background-layer"></div>
+            <audio id="audio" ref={audioRef} loop> 
+                <source src={backgroundAudio} type="audio/mpeg" />
+            </audio>
         </div>
     )
 }
