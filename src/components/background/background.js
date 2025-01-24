f0f34f099eeee7952fd5de6608260e7474fe610e
Sumit <nupzmit@Nupzmits-MacBook-Pro.local>
2025-01-24 18:51:50 +0100

    First line of background component

---
+++ src/components/background/background.js
@@ -0,0 +1,10 @@
+function Background() {
+    return (
+        <div className="container">
+            <div className="background-layer-1"></div>
+            <div className="background-layer-2"></div>
+        </div>
+    )
+}
+
+export default Background;
