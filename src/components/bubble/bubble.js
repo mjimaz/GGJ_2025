62de9508bfb52f80617d1067d3440f4e9f295773
Maryam <m.aljimaz@gmail.com>
2025-01-26 16:25:32 +0000

    style bubbles

--- src/components/bubble/bubble.js
+++ src/components/bubble/bubble.js
@@ -3,19 +3,23 @@ import "./bubble.css";
 import bubble_img from './bubble.png';
 
 function Bubble(props) {
-
+const divRef = useRef(null);
         const windowMaxWidth = window.innerWidth;
         const [startPosition, setStartPosition] = useState({ top: props.startY, left: props.startX });
 
         useEffect(() => {
             if(startPosition.startX >= windowMaxWidth) return;
                 setStartPosition({ top: props.startY , left: props.startX + 10});
+                if (divRef.current) {
+                    divRef.current.style.left = `${startPosition.left}px`;
+                }
             }, [startPosition]); 
 
 
 
     return (
         <div
+          ref={divRef}
           id="bubble"
           className="bubble"
           style = {{ 
