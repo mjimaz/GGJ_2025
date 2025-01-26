b33f51ef5ada1bfdd2cabc5736cec1fa1b0e1088
Maryam <m.aljimaz@gmail.com>
2025-01-26 14:01:16 +0000

    Set correct starting point

--- src/components/submarine/submarine.js
+++ src/components/submarine/submarine.js
@@ -8,7 +8,7 @@ function Submarine() {
   let submarineElement = useRef(null);
   const windowMaxHeight = window.innerHeight;
   const windowMaxWidth = window.innerWidth;
-  const [top, setTop] = useState(windowMaxHeight - 10);
+  const [top, setTop] = useState(windowMaxHeight - 250);
   const [left, setLeft] = useState(10);
    
 
