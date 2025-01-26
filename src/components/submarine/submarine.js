432fd3cec3344d228a2100e9d9be60b957e92b0d
Maryam <m.aljimaz@gmail.com>
2025-01-26 10:54:01 +0000

    debugging

--- src/components/submarine/submarine.js
+++ src/components/submarine/submarine.js
@@ -5,18 +5,22 @@ function Submarine() {
   let top = 0;
   let left = 0;
     function handleKeyDown(event){
-		console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
+		// console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
 
       let e = event.keyCode;
           
-          if (e == 40) { //down function
+          if (e === 40) { //down function
               top = top + 10;
-          } else if (e == 37) { //left function
+                 console.log('*** down - top $top');
+          } else if (e === 37) { //left function
             left = left - 10;
-          } else if (e == 39) { //right function
+            console.log('*** left - left $left');
+          } else if (e === 39) { //right function
            left = left+ 10 ;
-          } else if (e == 38) { //up function
+           console.log('*** right - left $left');
+          } else if (e === 38) { //up function
             top = top - 10 ;
+            console.log('*** up - top $top');
           }
     }
     document.addEventListener('keydown', handleKeyDown);
