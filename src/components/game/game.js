0ab1cd9288ba2f567616a327a5cbede1d409499b
Sumit <nupzmit@Nupzmits-MacBook-Pro.local>
2025-01-26 17:21:49 +0100

    Add a collision area on top of the submarine

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -94,10 +94,10 @@ function Game() {
       const rect1 = el1.getBoundingClientRect();
       const rect2 = submarine.getBoundingClientRect();
 
-      return rect1.left < (rect2.right * 0.5) &&
-        rect1.right > (rect2.left * 0.5) &&
-        rect1.top < (rect2.bottom * 0.5) &&
-        rect1.bottom > (rect2.top * 0.5);
+      return rect1.left < rect2.right &&
+        rect1.right > rect2.left &&
+        rect1.top < rect2.bottom &&
+        rect1.bottom > rect2.top;
     }
 
     function onGameStart() {
