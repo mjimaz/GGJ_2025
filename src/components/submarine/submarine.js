da936dc4c54d5336adf1342f0ce0e85eea3f5914
Maryam <m.aljimaz@gmail.com>
2025-01-26 10:26:56 +0000

    fix issues

--- src/components/submarine/submarine.js
+++ src/components/submarine/submarine.js
@@ -1,26 +1,32 @@
 import { useEffect, useState, useRef } from "react";
 
 function Submarine() {
+  const submarineElement = useRef(null);
+  const top = 0;
+  const left = 0;
     function handleKeyDown(event){
 		console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
 
       const e = event.keyCode;
-          const submarineElement = useRef(null);
-      
+          
           if (e == 40) { //down function
-              submarineElement.style.top = (parseInt(submarineElement.style.top)) + 10 + "px";
+              top = top + 10;
           } else if (e == 37) { //left function
-            submarineElement.style.left = (parseInt(submarineElement.style.left)) - 10 + "px";
+            left = left - 10;
           } else if (e == 39) { //right function
-            submarineElement.style.left = (parseInt(submarineElement.style.left)) + 10 + "px";
+           left = left+ 10 ;
           } else if (e == 38) { //up function
-            submarineElement.style.top = (parseInt(submarineElement.style.top)) - 10 + "px";
+            top = top - 10 ;
           }
     }
     document.addEventListener('keydown', handleKeyDown);
 
     return (
-        <div id="submarine">submarine</div>
+        <div ref={submarineElement}
+        style = {{top: '${top}px',
+          left: '${left}px' 
+        }}
+        >submarine</div>
     )
 }
 
