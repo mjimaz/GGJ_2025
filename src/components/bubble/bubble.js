9e0d312979ae22c47faf6e70a4c5b39e9a1f2a9e
Maryam <m.aljimaz@gmail.com>
2025-01-26 16:09:19 +0000

    animate bubbles

--- src/components/bubble/bubble.js
+++ src/components/bubble/bubble.js
@@ -2,13 +2,24 @@ import "./bubble.css";
 import bubble_img from './bubble.png';
 
 function Bubble(props) {
+
+        const windowMaxWidth = window.innerWidth;
+        const [startPosition, setStartPosition] = useState({ top: props.startY, left: props.startX });
+
+        useEffect(() => {
+    if(startPosition.startX >= windowMaxWidth) return;
+            setStartPosition({ top: startY , left: startX + 10});
+        }, [startPosition]); 
+
+
+
     return (
         <div
           id="bubble"
           className="bubble"
           style = {{ 
-            top: `${props.top}px`,
-            left: `${props.left}px`,
+            top: `${startPosition.top}px`,
+            left: `${startPosition.left}px`,
             background: `url("${bubble_img}") no-repeat`,}}
         ></div>
     )
